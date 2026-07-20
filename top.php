ebffdc61bd36009833f213b0d61aa4e754409d51
“YukiYamada1031” <office.deborah.0217@gmail.com>
2026-07-21 00:07:19 +0900

    set up second

--- top.php
+++ top.php
@@ -1,3 +1,21 @@
 <?php
-echo 'deborah mae viador de la cruze';
+
+$grname = 'deborah mae viador de la cruze';
+
+$name= 'yuki yamada';
+
+echo '私の名前は、'.$name.'です。'
+
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
