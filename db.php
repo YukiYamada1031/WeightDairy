d11ccc537dcd6b57a44ecd08f8505d38cd736fa8
“YukiYamada1031” <office.deborah.0217@gmail.com>
2026-08-01 00:30:30 +0900

    try to fetch data from DB in index.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $db_host = 'localhost';
 $db_name = 'weight_dairy';
 $db_user = 'root';
-$db_pass = ''
+$db_pass = 'root';
 $db_charset = 'utf8mb4';
 
 // DSN（Data Source Name）の作成
@@ -12,13 +12,13 @@ $dsn = 'mysql:host='.$db_host.';dbname='.$db_name.';charset='.$db_charset;
 // PDOのオプション設定
 $option = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH => PDO::FETCH_ASSOC,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     // SQLインジェクション対策
-    PDO::ARRT_EMULATE_PREPARES => false,    
+    PDO::ATTR_EMULATE_PREPARES => false,    
 );
 
 try {
-    $pdo = new PDO($dsn, $db_user, $db_pass $option);
+    $pdo = new PDO($dsn, $db_user, $db_pass, $option);
 } catch (PDOException $e) {
     exit('DB接続エラー:'. $e->getMessage());
 }
--- index.php
+++ index.php
@@ -1,10 +1,37 @@
 <?php
+require_once('db.php');
 
 $Button = "
   <div id='index-button'>
     <button class='start-button' onclick=\"location.href='record.php'\">本日の記録を始める</button>
     <button class='end-button' onclick=\"location.href='top.php'\">トップ画面に戻る</button>
   </div>
+";
+
+$sql = 'SELECT * FROM weight_record ORDER BY record_day ASC';
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$results = $stmt->fetchAll();
+
+$touroku = '';
+foreach($results as $result) {
+  $day = $result['record_day'];
+  $weight = $result['weight'];
+
+  $touroku .= '<tr>';
+  $touroku .= '<td>' . $day . '</td>';
+  $touroku .= '<td>' . $weight . ' kg</td>';
+  $touroku .= '</tr>';
+}
+
+$frame = "
+  <table>
+    <tr>
+      <th>日付</th>
+      <th>体重</th>
+    </tr>
+    {$touroku}
+  </table>
 ";
 
 ?>
@@ -25,8 +52,7 @@ $Button = "
     </div>
 
     <?php print $Button ?>
-
-
+    <?php print $frame ?>
   </div>
 </body>
 </html>
